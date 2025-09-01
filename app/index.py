0d25bf2b0b8d94b6a8da4a7856d77225dff6732c
phuongthao102 <2251010084thao@ou.edu.vn>
2025-09-01 19:33:47 +0700

    update search

--- app/index.py
+++ app/index.py
@@ -35,6 +35,9 @@ print("c")
 print("c")
 print("c")
 print("c")
+print("c")
+print("c")
+print("c")
 if __name__ == '__main__':
 #  with app.app_context():
         # db.create_all()   # Tạo tất cả bảng trong database
